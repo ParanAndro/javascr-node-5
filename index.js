5dc7c4dc450d08d6107419376ead8066a7c08908
Radek Izdebski <radek1izdebski@gmail.com>
2018-03-05 20:23:01 +0100

    Add html and js files

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-var http = require('http')
-	fs = require('fs');
+var http = require('http'),
+	fs = require('fs'),
+	server = http.createServer()
 
-var server = http.createServer();
+
 server.on('request', function (request, response) { //eventEmmiter ustaiony na nasłuchiwanie odpytywania klienta
-    response.write('Hello world!');
-    response.end();
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    if (request.method === 'GET' && request.url === '/index') {
+    	fs.readFile('./index.html', 'utf-8', function(err, data) {
+    	response.write(data);
+    		response.end();
+    	});
+    } else {
+    	response.statusCode = 404;
+    	response.write('<h1>404: Zła ścieżka!<h1>');
+    	fs.readFile('./cup.jpg', function(err, data) {
+    		if (err) throw err;
+    		response.write(data);
+    	});
+    		response.end();
+    }
 });
 server.listen(9000);
+
+
