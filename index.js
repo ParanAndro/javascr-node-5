2e808d052c79c3326802dd6b025c38e740ced441
Radek Izdebski <radek1izdebski@gmail.com>
2018-03-13 19:47:42 +0100

    Change part of index.js - case statusCode 404

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 var http = require('http'),
 	fs = require('fs'),
-	express = require('express'),
 	server = http.createServer()
 
-
 server.on('request', function (request, response) { //eventEmmiter ustaiony na nasłuchiwanie odpytywania klienta
-    response.setHeader('Content-Type', 'text/html; charset=utf-8');
-    if (request.method === 'GET' && request.url === '/index') {
-    	fs.readFile('./index.html', 'utf-8', function(err, data) {
-    	response.write(data);
-    	});
-    } else {
-    	var app = express();
-    	response.statusCode = 404;
-    	response.write('<h1>404: Zła ścieżka!<h1>');
-    	app.get(/*'?'*/, function(req, res) {
-			res.sendFile('./cup.jpg');
-			console.log('ok');
-            response.end();
-	});
-    	 
-    }
+
+	if (request.method === 'GET' && request.url === '/index') {
+		response.setHeader('Content-Type', 'text/html; charset=utf-8');
+		fs.readFile('./index.html', 'utf-8', function(err, data) {
+			response.write(data);
+			response.end();
+		});
+	} else {
+	response.statusCode = 404;
+		fs.readFile('./cup.jpg', function(err, data) {
+			if (err) throw err;
+			response.write(data);
+			response.end();
+		});
+	};
 });
+
 server.listen(9000);
-
-
